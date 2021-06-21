b49950a84ea9f5c60d4f5f7d49f38a0068d0607b
GODZILLA <85907312+GODZILLA66640@users.noreply.github.com>
2021-06-20 23:15:54 -0400

    Update Image Converter.bat

--- Image Converter.bat
+++ Image Converter.bat
@@ -24,11 +24,36 @@ IF ERRORLEVEL 1 GOTO JPEG-PNG
 
 :JPEG-PNG
 if exist *.jpeg goto JPEG-PNG-True
+if exist *.jpg goto 11
+if exist *.jpe goto 12
+if exist *.jif goto 13
+if exist *.jfif goto 14
+if exist *.jjfi goto 15
 if exist *.png goto wrong input
 if not exist *.jpeg goto kill
+if not exist *.jpg goto kill
+if not exist *.jpe goto kill
+if not exist *.jif not goto kill
+if not exist *.jfif not goto kill
+if not exist *.jjfi goto kill
 
 :JPEG-PNG-True
 rename *.jpeg *.png
+goto end
+:11
+rename *.jpg *.png
+goto end
+:12
+rename *.jpe *.png
+goto end
+:13
+rename *.jif *.png
+goto end
+:14
+rename *.jfif *.png
+goto end
+:15
+rename *.jjfi *.png
 goto end
 
 :PNG-JPEG
